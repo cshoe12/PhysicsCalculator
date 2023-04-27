26be6bdb1f932f436097e2fa92882c8151d6b8e3
shoemakerc24 <shoemakerc24@hsnet.ahsd.org>
2023-04-27 10:37:45 -0400

    units ui pt 2

--- app/src/main/java/com/example/physicscalculator/TimeFragment.kt
+++ app/src/main/java/com/example/physicscalculator/TimeFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.physicscalculator.databinding.FragmentMassBinding
+
 import com.example.physicscalculator.databinding.FragmentTimeBinding
 
 class TimeFragment : Fragment() {
--- app/src/main/java/com/example/physicscalculator/VolumeFragment.kt
+++ app/src/main/java/com/example/physicscalculator/VolumeFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.physicscalculator.databinding.FragmentTimeBinding
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.physicscalculator.databinding.FragmentVolumeBinding
 
 class VolumeFragment : Fragment() {
 
-    private var _binding: FragmentTimeBinding? = null
+    private var _binding: FragmentVolumeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -17,8 +19,14 @@ class VolumeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentTimeBinding.inflate(inflater, container, false)
+        _binding = FragmentVolumeBinding.inflate(inflater, container, false)
         val rootView = binding.root
+
+        val unitsArrayAdapter = ArrayAdapter.createFromResource(requireActivity(), R.array.volume_units,
+            android.R.layout.simple_spinner_item)
+        unitsArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+       binding.VinitialSpinner.adapter = unitsArrayAdapter
+
 
         return rootView
     }
