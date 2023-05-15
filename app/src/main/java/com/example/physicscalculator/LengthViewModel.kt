df887d94071828d4d4069cfa5698d154f4fb5cd5
shoemakerc24 <shoemakerc24@hsnet.ahsd.org>
2023-05-15 10:38:20 -0400

    logic

--- app/src/main/java/com/example/physicscalculator/LengthViewModel.kt
+++ app/src/main/java/com/example/physicscalculator/LengthViewModel.kt
@@ -28,40 +28,110 @@ class LengthViewModel: ViewModel() {
         _initVal = strVal
     }
 
-    fun calculate(): Double{
-        var converted: Double = 0.0
-        if(_initUnit == "m"){
-            if(_finalUnit =="cm"){
-                converted = _initVal.toDouble() * 100
-            }
+    fun toMeters(unit: String, init : Double): Double{
+        var meters = 0.0
+        if(unit =="cm"){
+            meters = init * 100
+        }
+        else if(unit =="km"){
+            meters = _initVal.toDouble() / 1000
+        }
+        else if(unit =="mm"){
+            meters = _initVal.toDouble() * 1000
+        }
+        else if(unit =="micro-m"){
+            meters = _initVal.toDouble() * 1000000
+        }
+        else if(unit =="nano-m"){
+            meters = _initVal.toDouble() * 1000000000
+        }
+        else if(unit =="ft"){
+            meters = (_initVal.toDouble() * 39.37) / 12
+        }
+        else if(unit =="in"){
+            meters = _initVal.toDouble() * 39.37
+        }
+        else if(unit =="yd"){
+            meters = (_initVal.toDouble() * 39.37) / 36
+
+        }
+        else if(unit =="mi"){
+            meters = (_initVal.toDouble() / 1000) * 0.621
+        }
+        else if(unit =="angstrom"){
+            meters = (_initVal.toDouble() * 39.37) * 4000000000
+        }
+        return meters
+    }
+
+//    fun fromMeters(num: Double): Double{
+//        var final = 0.0
+//        if(_finalUnit =="cm"){
+//            final = num /
+//        }
+//        else if(_finalUnit =="km"){
+//            converted = toMeters("km")
+//        }
+//        else if(_finalUnit =="mm"){
+//            converted = toMeters("mm")
+//        }
+//        else if(_finalUnit =="micro-m"){
+//            converted = toMeters("micro-m")
+//        }
+//        else if(_finalUnit =="nano-m"){
+//            converted = toMeters("nano-m")
+//        }
+//        else if(_finalUnit =="ft"){
+//            converted = toMeters("ft")
+//        }
+//        else if(_finalUnit =="in"){
+//            converted = toMeters("in")
+//        }
+//        else if(_finalUnit =="yd"){
+//            converted = toMeters("yd")
+//        }
+//        else if(_finalUnit =="mi"){
+//            converted = toMeters("mi")
+//        }
+//        else if(_finalUnit =="angstrom"){
+//            converted = toMeters("angstrom")
+//        }
+//    }
+
+//    fun calculate(): Double{
+//        var converted: Double = 0.0
+//        if(_initUnit == "m"){
+//            if(_finalUnit =="cm"){
+//                converted = toMeters("cm")
+//            }
 //            else if(_finalUnit =="km"){
-//
+//                converted = toMeters("km")
 //            }
 //            else if(_finalUnit =="mm"){
-//
+//                converted = toMeters("mm")
 //            }
 //            else if(_finalUnit =="micro-m"){
-//
+//                converted = toMeters("micro-m")
 //            }
 //            else if(_finalUnit =="nano-m"){
-//
+//                converted = toMeters("nano-m")
 //            }
 //            else if(_finalUnit =="ft"){
-//
+//                converted = toMeters("ft")
 //            }
 //            else if(_finalUnit =="in"){
-//
+//                converted = toMeters("in")
 //            }
 //            else if(_finalUnit =="yd"){
-//
+//                converted = toMeters("yd")
 //            }
 //            else if(_finalUnit =="mi"){
-//
+//                converted = toMeters("mi")
 //            }
 //            else if(_finalUnit =="angstrom"){
-//
+//                converted = toMeters("angstrom")
 //            }
-        }
+//        }
 
 //        if(_initUnit == "cm"){
 //            if(_finalUnit =="m"){
