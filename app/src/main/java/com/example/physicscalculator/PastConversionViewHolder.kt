f08e77ecf0d597e271e4f20632bf4e25530f8bc1
shoemakerc24 <shoemakerc24@hsnet.ahsd.org>
2023-05-30 15:19:20 -0400

    recycler view

---
+++ app/src/main/java/com/example/physicscalculator/PastConversionViewHolder.kt
@@ -0,0 +1,18 @@
+package com.example.physicscalculator
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.physicscalculator.databinding.ListItemLayoutBinding
+
+class PastConversionViewHolder(val binding: ListItemLayoutBinding):
+    RecyclerView.ViewHolder(binding.root) {
+
+    private lateinit var currentPastConversion: PastConversion
+
+    fun bindPastConversion(pc: PastConversion){
+        currentPastConversion = pc
+        binding.iUnitTextView.text = pc.initialUnit
+        binding.fUnitTextView.text = pc.finalUnit
+    }
+
+
+}
